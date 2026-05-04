23c64e455478c5ebb044d1f25f4900e87f546b81
lick50001 <mishakazakov82@gmail.com>
2026-05-04 14:43:30 +0500

    Разработанные контексты данных. Марков

---
+++ ShopManager_Markov/Models/Client.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+using ShopManager_Markov.Classes;
+using System.Windows;
+
+namespace ShopManager_Markov.Models
+{
+    public class Client : Notification
+    {
+        public int Id { get; set; }
+
+        private string fullName;
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,100}$");
+                if (!match.Success)
+                    MessageBox.Show("ФИО слишком большое!", "Ошибка");
+                else
+                {
+                    fullName = value;
+                    OnPropertyChanged("FullName");
+                }
+            }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string address;
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        private decimal totalPurchases;
+        public decimal TotalPurchases
+        {
+            get { return totalPurchases; }
+            set
+            {
+                totalPurchases = value;
+                OnPropertyChanged("TotalPurchases");
+                OnPropertyChanged("TotalPurchasesText");
+            }
+        }
+
+        [Schema.NotMapped]
+        private bool isEnable;
+        [Schema.NotMapped]
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+                OnPropertyChanged("IsEnableText");
+            }
+        }
+
+        [Schema.NotMapped]
+        public string IsEnableText
+        {
+            get { return IsEnable ? "Сохранить" : "Изменить"; }
+        }
+
+        [Schema.NotMapped]
+        public string TotalPurchasesText
+        {
+            get { return $"{TotalPurchases:C}"; }
+        }
+
+        [Schema.NotMapped]
+        public RelayCommand OnEdit
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    IsEnable = !IsEnable;
+                    if (!IsEnable)
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_clients.clientsContext.SaveChanges();
+                });
+            }
+        }
+
+        [Schema.NotMapped]
+        public RelayCommand OnDelete
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    if (MessageBox.Show("Удалить этого клиента?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_clients.Clients.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_clients.clientsContext.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_clients.clientsContext.SaveChanges();
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ ShopManager_Markov/Models/Product.cs
@@ -0,0 +1,144 @@
+﻿using ShopManager_Markov.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopManager_Markov.Models
+{
+    public class Product : Notification
+    {
+        public int Id { get; set; }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                Match match = Regex.Match(value, "^.{1,100}$");
+                if (!match.Success)
+                    MessageBox.Show("Название должно быть не более 100 символов!", "Ошибка");
+                else
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private decimal price;
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                    MessageBox.Show("Цена не может быть отрицательной!", "Ошибка");
+                else
+                {
+                    price = value;
+                    OnPropertyChanged("Price");
+                    OnPropertyChanged("PriceText");
+                }
+            }
+        }
+
+        private int stock;
+        public int Stock
+        {
+            get { return stock; }
+            set
+            {
+                if (value < 0)
+                    MessageBox.Show("Количество не может быть отрицательным!", "Ошибка");
+                else
+                {
+                    stock = value;
+                    OnPropertyChanged("Stock");
+                }
+            }
+        }
+
+        private string category;
+        public string Category
+        {
+            get { return category; }
+            set
+            {
+                category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        [Schema.NotMapped]
+        private bool isEnable;
+        [Schema.NotMapped]
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+                OnPropertyChanged("IsEnableText");
+            }
+        }
+
+        [Schema.NotMapped]
+        public string IsEnableText
+        {
+            get { return IsEnable ? "Сохранить" : "Изменить"; }
+        }
+
+        [Schema.NotMapped]
+        public string PriceText
+        {
+            get { return $"{Price:C}"; }
+        }
+
+        [Schema.NotMapped]
+        public RelayCommand OnEdit
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    IsEnable = !IsEnable;
+                    if (!IsEnable)
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_products.productsContext.SaveChanges();
+                });
+            }
+        }
+
+        [Schema.NotMapped]
+        public RelayCommand OnDelete
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    if (MessageBox.Show("Удалить этот товар?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_products.Products.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_products.productsContext.Remove(this);
+                        (MainWindow.init.DataContext as ViewModels.VM_Pages).vm_products.productsContext.SaveChanges();
+                    }
+                });
+            }
+        }
+    }
+}
